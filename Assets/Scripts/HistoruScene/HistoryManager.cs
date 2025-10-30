2d953ea3ff2f621c3ac72bef52c3ea91c1b9a131
畑山直輝 <naoki42070@gmail.com>
2025-10-31 03:38:41 +0900

    [add]GoBackボタンの追加

--- Assets/Scripts/HistoruScene/HistoryManager.cs
+++ Assets/Scripts/HistoruScene/HistoryManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HistoryManager : MonoBehaviour
 {
+    [SerializeField] private Button backButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        backButton.onClick.AddListener(GameManager.Instance.GoToLab);
     }
 
     // Update is called once per frame
