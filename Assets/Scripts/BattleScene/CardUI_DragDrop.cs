6715dd3e5bc6ae5c466897b3e46cf974fa460078
唐住拓茉 <211954539+KarasumiTakuma@users.noreply.github.com>
2025-11-08 17:36:35 +0900

    [update]CardUI_DragDrop.cs、HandAreaManager.cs、BattleManager.cs

--- Assets/Scripts/BattleScene/CardUI_DragDrop.cs
+++ Assets/Scripts/BattleScene/CardUI_DragDrop.cs
@@ -3,19 +3,27 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
 
+/// カードのドラッグ＆ドロップ挙動を制御するクラス。
 public class CardUI_DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
     private Card cardData;
-    private BattleManager battleManager;
-    private Transform originalParent;
+    private Transform originalParent; 
     private Vector3 originalPosition;
     private Canvas canvas;  // ドラッグ時にUIが隠れないようにする
 
-    public void Setup(Card card, BattleManager manager)
+    // カードプレイを通知するイベント
+    // Card型の引数を1つ受け取りvoidを返すメソッド(イベント群)を格納するもの
+    // メソッドを変数のように扱える型がdelegate
+    public delegate void CardPlayedHandler(Card card);
+
+    // CardPlayedHandler型の変数OnCardPlayedを「イベント」として宣言
+    // 他のクラスからこのイベント変数にメソッドを登録できる。
+    public event CardPlayedHandler OnCardPlayed;
+
+    public void Setup(Card card)
     {
         cardData = card;
-        battleManager = manager;
         canvas = GetComponentInParent<Canvas>();
     }
 
@@ -36,14 +44,16 @@ public class CardUI_DragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     // ドラッグ終了
     public void OnEndDrag(PointerEventData eventData)
     {
+
         // 仮に画面中央より上でドロップしたらカードをプレイする
         if (eventData.position.y > Screen.height / 2f)
         {
-            if (battleManager != null)
-            {
-                battleManager.PlayCard(cardData);
-            }
-            Destroy(gameObject); // カードをUIから削除
+            // イベント変数OnCardPlayedに登録されているメソッドを順番に呼び出す
+            // null条件演算子「?」でOnCardPlayedにメソッドが登録されているか(nullでないか)を確認
+            // Invokeはイベントに登録されたメソッドをまとめて呼び出すメソッド。
+            // 引数としてcardDataを渡すことで、登録されているメソッドすべてCard型の引数cardDataを受け取るこのになる。
+            OnCardPlayed?.Invoke(cardData);  
+            Destroy(gameObject); // カードをUIから削除   
         }
         else
         {
--- Assets/Scripts/BattleScene/HandAreaManager.cs
+++ Assets/Scripts/BattleScene/HandAreaManager.cs
@@ -6,14 +6,21 @@ using TMPro;
 public class HandAreaManager : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private DeckManager deckManager;
     [SerializeField] private GameObject cardPrefab;
-    [SerializeField] private Transform handArea;
-    [SerializeField] private BattleManager battleManager; // カードプレイ処理に必要
+    [SerializeField] private Transform[] cardSlots; // ← 各スロット(5枚のカードを配置する場所)を個別に指定
 
     private readonly List<GameObject> spawnedCards = new List<GameObject>();
+    private List<Card> handCardData;
 
-    public void UpdateHandUI()
+    public void setHandCardData(List<Card> handCardData)
+    {
+        this.handCardData = handCardData;
+    }
+
+    //  引数にCard型変数を受け取るようなメソッドたちを登録しておく変数がonCardPlayed
+    //  Action は「戻り値がないメソッド」を表す型
+    //  Action<Care>は、「引数がCard型である返り値のないメソッドの型」を意味する
+    public void UpdateHandUI(System.Action<Card> onCardPlayed)
     {
         // 1. 既存UI削除
         foreach (var card in spawnedCards)
@@ -22,37 +29,66 @@ public class HandAreaManager : MonoBehaviour
         }
         spawnedCards.Clear();
 
-        // 2. 新規UI生成
-        foreach (var cardData in deckManager.GetHand())
-        {
-            GameObject cardObj = Instantiate(cardPrefab, handArea);
+        // 2. 各スロットの位置に応じてカード生成
 
-            // カード名
-            // ? は null 条件演算子。参照がnullじゃなければアクセスして、nullなら処理をスキップする
-            // TMP_Text型の構造体の.text変数にテキストを代入すると、それをUI表示してくれる
+        int dataIndex = 0;
+        foreach (var slotPoint in cardSlots)
+        {
+            // カードスロット5つに対して手札カードが4枚以下の場合に、ないはずの5枚目のデータ(空値)にアクセスしないようにする
+            if (dataIndex >= handCardData.Count) break;
+
+            // cardObjはそのスポーン位置(slotPoint)を親とする
+            GameObject cardObj = Instantiate(cardPrefab, slotPoint);
+
+            // cardObjオブジェとそのスポーンポイント(slotPoint)のRectTransformを取得
+            RectTransform cardRect = cardObj.GetComponent<RectTransform>();
+            RectTransform slotRect = slotPoint.GetComponent<RectTransform>();
+
+            if (cardRect != null && slotRect != null) //RectTransformが空値でないかを確認
+            {
+                //Inspector上で各オブジェクトのサイズ設定を誤っても正しいサイズにするため
+
+                // card(手札カード)オブジェクトのサイズをslotPointに合わせる
+                cardRect.sizeDelta = slotRect.sizeDelta;
+
+                // 各手札カードオブジェクトの位置をそれぞれの親であるslotPointのpivot位置(中心)にする
+                cardRect.anchoredPosition = Vector2.zero;
+
+                // 手札カードのサイズは1に固定
+                cardRect.localScale = Vector3.one;
+            }
+
+            Card cardData = handCardData[dataIndex];
+
             TMP_Text nameText = cardObj.transform.Find("NameText")?.GetComponent<TMP_Text>();
             if (nameText) nameText.text = cardData.GetName();
 
-            // マナコスト
             TMP_Text manaText = cardObj.transform.Find("ManaCostText")?.GetComponent<TMP_Text>();
-            if (manaText) manaText.text = "Cost：" + cardData.GetManaCost().ToString();
+            if (manaText) manaText.text = "Cost：" + cardData.GetManaCost();
 
-            // 攻撃力
             TMP_Text powerText = cardObj.transform.Find("PowerText")?.GetComponent<TMP_Text>();
-            if (powerText) powerText.text = "Att" + cardData.GetPower().ToString();
+            if (powerText) powerText.text = "Att" + cardData.GetPower();
 
-            // 画像
             Image cardImage = cardObj.transform.Find("CardImage")?.GetComponent<Image>();
             if (cardImage) cardImage.sprite = cardData.GetSprite();
 
-            // CardUI_DragDropをセットアップ
             var dragDrop = cardObj.GetComponent<CardUI_DragDrop>();
-            if (dragDrop != null)
+            if (dragDrop != null && onCardPlayed != null)
             {
-                dragDrop.Setup(cardData, battleManager);
+                dragDrop.Setup(cardData);
+                // イベント変数にイベントを登録
+                // onCardPlayed変数には、UpdateHandUI()メソッドが呼び出された際に
+                // 引数として渡されたメソッド(引数：Card型、返り値：void)が登録されているはずである(BattleManagerならPlayCard)
+                // なお、onCardPlayedはAction<Card>型であるので、
+                // CardUI_DragDropのCardPlayedHandler(カードプレイを通知するイベント群を登録する場所)に
+                // 一時的に登録して、それをイベント変数dragDrop.OnCardPlayedにイベントとして登録する
+                dragDrop.OnCardPlayed += new CardUI_DragDrop.CardPlayedHandler(onCardPlayed);
             }
 
             spawnedCards.Add(cardObj);
+            dataIndex++;
         }
     }
+    // 他クラスから生成したカードUIリスト取得
+    public List<GameObject> GetSpawnedCards() => spawnedCards;
 }
